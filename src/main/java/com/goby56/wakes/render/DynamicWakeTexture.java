6ba2bbdd65e92779546bda7c3406261fc5c7d0da
Goby56 <60710855+Goby56@users.noreply.github.com>
2024-04-02 17:58:14 +0200

    start to LOD + per pixel blending
    broken LOD system but improved blending which halves the number of draw calls for wakes

---
+++ src/main/java/com/goby56/wakes/render/DynamicWakeTexture.java
@@ -0,0 +1,109 @@
+package com.goby56.wakes.render;
+
+import com.goby56.wakes.WakesClient;
+import com.goby56.wakes.render.enums.RenderType;
+import com.goby56.wakes.render.enums.WakeColor;
+import com.goby56.wakes.simulation.WakeNode;
+import com.mojang.blaze3d.platform.GlConst;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.platform.TextureUtil;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.*;
+import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL14;
+import org.lwjgl.system.MemoryUtil;
+
+import java.util.ArrayList;
+
+public class DynamicWakeTexture {
+
+    private static DynamicWakeTexture INSTANCE = null;
+    private final ArrayList<Texture> lods = new ArrayList<>();
+    private Texture currentTexture = null;
+
+    private DynamicWakeTexture() {
+        for (int i = 5; i >= 0; i--) {
+            lods.add(new Texture((int) Math.pow(2, i)));
+        }
+    }
+
+    public static DynamicWakeTexture getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new DynamicWakeTexture();
+        }
+        return INSTANCE;
+    }
+
+    private static int distToLOD(float distance) {
+        int pow = WakesClient.CONFIG_INSTANCE.wakeResolution.power;
+        return (int) Math.min(5, Math.floor(pow * (Math.exp(distance / (16 * pow)) - 2) + 5));
+    }
+
+    public void populatePixels(WakeNode node, float distance, int waterColor, float opacity) {
+
+        int lod = distToLOD(distance);
+        Texture texture = lods.get(lod);
+        int samples = Math.max(1, node.res / texture.res);
+        for (int i = 0; i < texture.res; i += samples) {
+            for (int j = 0; j < texture.res; j += samples) {
+                float avg = 0;
+                for (int dy = 0; dy < samples; dy++) {
+                    for (int dx = 0; dx < samples; dx++) {
+                        avg = (node.u[0][i + dy + 1][j + dx + 1] + node.u[1][i + dy + 1][j + dx + 1] + node.u[2][i + dy + 1][j + dx + 1]) / 3;
+                    }
+                }
+                int color = WakeColor.getColor(avg / (samples * samples), waterColor, opacity);
+                MemoryUtil.memPutInt(texture.imgPtr + (((i*(long)node.res)+j)*4), color);
+            }
+        }
+        this.currentTexture = texture;
+    }
+
+    public void render(Matrix4f matrix, float x, float y, float z, float r, float g, float b, float a, int light) {
+        GlStateManager._bindTexture(currentTexture.glTexId);
+        GlStateManager._pixelStore(GlConst.GL_UNPACK_ROW_LENGTH, 0);
+        GlStateManager._pixelStore(GlConst.GL_UNPACK_SKIP_PIXELS, 0);
+        GlStateManager._pixelStore(GlConst.GL_UNPACK_SKIP_ROWS, 0);
+        GlStateManager._pixelStore(GlConst.GL_UNPACK_ALIGNMENT, 4);
+        GlStateManager._texSubImage2D(GlConst.GL_TEXTURE_2D, 0, 0, 0, currentTexture.res, currentTexture.res, GlConst.GL_RGBA, GlConst.GL_UNSIGNED_BYTE, currentTexture.imgPtr);
+
+        RenderSystem.setShaderTexture(0, currentTexture.glTexId);
+
+        RenderSystem.setShader(RenderType.getProgram());
+
+        BufferBuilder buffer = Tessellator.getInstance().getBuffer();
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL);
+
+        buffer.vertex(matrix, x, y, z).color(r, g, b, a).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
+        buffer.vertex(matrix, x, y, z + 1).color(r, g, b, a).texture(0, 1).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
+        buffer.vertex(matrix, x + 1, y, z + 1).color(r, g, b, a).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
+        buffer.vertex(matrix, x + 1, y, z).color(r, g, b, a).texture(1, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
+
+        Tessellator.getInstance().draw();
+    }
+
+    private static class Texture {
+        public int res;
+        public int glTexId;
+        public long imgPtr;
+
+        public Texture(int resolution) {
+            this.res = resolution;
+            this.glTexId = TextureUtil.generateTextureId();
+            this.imgPtr = MemoryUtil.nmemAlloc((long) resolution * resolution * 4);
+
+            GlStateManager._bindTexture(this.glTexId);
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAX_LEVEL, 0);
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MIN_LOD, 0);
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAX_LOD, 0);
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, 0f);
+
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MIN_FILTER, GL12.GL_NEAREST);
+            GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAG_FILTER, GL12.GL_NEAREST);
+
+            GlStateManager._texImage2D(GlConst.GL_TEXTURE_2D, 0, GlConst.GL_RGBA, resolution, resolution, 0, GlConst.GL_RGBA, GlConst.GL_UNSIGNED_BYTE, null);
+        }
+
+    }
+}
--- src/main/java/com/goby56/wakes/render/WakeTextureRenderer.java
+++ src/main/java/com/goby56/wakes/render/WakeTextureRenderer.java
@@ -2,13 +2,8 @@ package com.goby56.wakes.render;
 
 import com.goby56.wakes.WakesClient;
 import com.goby56.wakes.render.enums.BlendingFunction;
-import com.goby56.wakes.render.enums.RenderType;
-import com.goby56.wakes.render.enums.WakeColor;
 import com.goby56.wakes.simulation.WakeHandler;
 import com.goby56.wakes.simulation.WakeNode;
-import com.mojang.blaze3d.platform.GlConst;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.platform.TextureUtil;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
@@ -17,10 +12,6 @@ import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.client.render.*;
 import net.minecraft.util.math.Vec3d;
 import org.joml.Matrix4f;
-import org.lwjgl.opengl.GL12;
-import org.lwjgl.opengl.GL14;
-import org.lwjgl.system.MemoryUtil;
-
 import java.util.ArrayList;
 
 public class WakeTextureRenderer implements WorldRenderEvents.AfterTranslucent {
@@ -41,82 +32,44 @@ public class WakeTextureRenderer implements WorldRenderEvents.AfterTranslucent {
 
         BlendingFunction.applyBlendFunc();
 
-        // TODO MOVE THIS TO RESOLUTION CHANGE EVENT
-        int res = wakeHandler.resolution.res;
-        if (wakeHandler.glFoamTexId == -1) {
-            wakeHandler.glFoamTexId = initTexture(res);
-        }
-        if (wakeHandler.glWakeTexId == -1) {
-            wakeHandler.glWakeTexId = initTexture(res);
-        }
-        if (wakeHandler.foamImgPtr == -1) {
-            wakeHandler.foamImgPtr = MemoryUtil.nmemAlloc((long) res * res * 4);
-        }
-        if (wakeHandler.wakeImgPtr == -1) {
-            wakeHandler.wakeImgPtr = MemoryUtil.nmemAlloc((long) res * res * 4);
-        }
+        DynamicWakeTexture wakeTexture = DynamicWakeTexture.getInstance();
 
-        float avg;
         int waterCol;
-        WakeColor wakeCol;
         float r, g, b, a;
         float x, y, z;
         int light;
-        long fullTime = 0;
-        long t1 = 0;
+        long fullTime;
+        long t1;
         long memoryTime = 0;
-        long t2 = 0;
+        long t2;
         long renderingTime = 0;
-        long t3 = 0;
+        long t3;
 
         t1 = System.nanoTime();
 
         for (WakeNode node : nodes) {
             if (node.isDead()) continue;
-            if (res != node.res) continue;
+            if (wakeHandler.resolution.res != node.res) continue;
             Vec3d screenSpace = node.getPos().add(context.camera().getPos().negate());
             x = (float) screenSpace.x;
             y = (float) screenSpace.y;
             z = (float) screenSpace.z;
+            float distance = (float) Math.sqrt(screenSpace.lengthSquared());
 
             t2 = System.nanoTime();
 
-            for (int i = 0; i < res; i++) {
-                for (int j = 0; j < res; j++) {
-                    avg = (node.u[0][i+1][j+1] + node.u[1][i+1][j+1] + node.u[2][i+1][j+1]) / 3;
-                    wakeCol = WakeColor.getColor(avg);
-                    if (wakeCol == WakeColor.WHITE) {
-                        MemoryUtil.memPutInt(wakeHandler.foamImgPtr + (((i*(long)res)+j)*4), wakeCol.argb);
-                        MemoryUtil.memPutInt(wakeHandler.wakeImgPtr + (((i*(long)res)+j)*4), 0);
-                    } else {
-                        MemoryUtil.memPutInt(wakeHandler.foamImgPtr + (i*((long)res)+j)*4, 0);
-                        MemoryUtil.memPutInt(wakeHandler.wakeImgPtr + (i*((long)res)+j)*4, wakeCol.argb);
-                    }
-                }
-            }
-
-            memoryTime += System.nanoTime() - t2;
-
             waterCol = BiomeColors.getWaterColor(context.world(), node.blockPos());
             light = WorldRenderer.getLightmapCoordinates(context.world(), node.blockPos());
-
-            if (WakesClient.CONFIG_INSTANCE.useWaterBlending) {
-                r = (float) (waterCol >> 16 & 0xFF) / 255f;
-                g = (float) (waterCol >> 8 & 0xFF) / 255f;
-                b = (float) (waterCol & 0xFF) / 255f;
-            } else {
-                r = 1f;
-                g = 1f;
-                b = 1f;
-            }
             a = (float) ((-Math.pow(node.t, 2) + 1) * Math.pow(WakesClient.CONFIG_INSTANCE.wakeOpacity, WakesClient.CONFIG_INSTANCE.blendFunc.canVaryOpacity ? 1 : 0));
 
+            wakeTexture.populatePixels(node, distance, waterCol, a);
+
+            memoryTime += System.nanoTime() - t2;
 
             t3 = System.nanoTime();
 
             // TODO IMPLEMENT NODE TEXTURE RENDER CLUMPING (RENDER MULTIPLE NODES IN ONE QUAD/PASS)
-            renderTexture(res, wakeHandler.glWakeTexId, wakeHandler.wakeImgPtr, matrix, x, y, z, r, g, b, a, light);
-            renderTexture(res, wakeHandler.glFoamTexId, wakeHandler.foamImgPtr, matrix, x, y, z, 1f, 1f, 1f, a, light);
+            wakeTexture.render(matrix, x, y, z, 1f, 1f, 1f, 1f, light);
 
             renderingTime += System.nanoTime() - t3;
         }
@@ -125,47 +78,8 @@ public class WakeTextureRenderer implements WorldRenderEvents.AfterTranslucent {
         RenderSystem.enableCull();
 
         fullTime = System.nanoTime() - t1;
-        if (!nodes.isEmpty() && !MinecraftClient.getInstance().isPaused()) {
-            System.out.printf("Full time: %d, Memory time: %.2f, Rendering time: %.2f\n", fullTime, memoryTime / (float) fullTime, renderingTime / (float) fullTime);
-        }
-    }
-
-    private static void renderTexture(int resolution, int textureID, long texture, Matrix4f matrix, float x, float y, float z, float r, float g, float b, float a, int light) {
-        GlStateManager._bindTexture(textureID);
-        GlStateManager._pixelStore(GlConst.GL_UNPACK_ROW_LENGTH, 0);
-        GlStateManager._pixelStore(GlConst.GL_UNPACK_SKIP_PIXELS, 0);
-        GlStateManager._pixelStore(GlConst.GL_UNPACK_SKIP_ROWS, 0);
-        GlStateManager._pixelStore(GlConst.GL_UNPACK_ALIGNMENT, 4);
-        GlStateManager._texSubImage2D(GlConst.GL_TEXTURE_2D, 0, 0, 0, resolution, resolution, GlConst.GL_RGBA, GlConst.GL_UNSIGNED_BYTE, texture);
-
-        // TODO SWITCH TO STANDARD RENDER LAYERS (DIRECT DRAW CALLS MAY BE SLOW)
-        RenderSystem.setShaderTexture(0, textureID);
-
-        RenderSystem.setShader(RenderType.getProgram());
-
-        BufferBuilder buffer = Tessellator.getInstance().getBuffer();
-        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL);
-
-        buffer.vertex(matrix, x, y, z).color(r, g, b, a).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
-        buffer.vertex(matrix, x, y, z + 1).color(r, g, b, a).texture(0, 1).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
-        buffer.vertex(matrix, x + 1, y, z + 1).color(r, g, b, a).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
-        buffer.vertex(matrix, x + 1, y, z).color(r, g, b, a).texture(1, 0).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(0f, 1f, 0f).next();
-
-        Tessellator.getInstance().draw();
-    }
-
-    private static int initTexture(int resolution) {
-        int texId = TextureUtil.generateTextureId();
-        GlStateManager._bindTexture(texId);
-        GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAX_LEVEL, 0);
-        GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MIN_LOD, 0);
-        GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAX_LOD, 0);
-        GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, 0f);
-
-        GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MIN_FILTER, GL12.GL_NEAREST);
-        GlStateManager._texParameter(GlConst.GL_TEXTURE_2D, GL12.GL_TEXTURE_MAG_FILTER, GL12.GL_NEAREST);
-
-        GlStateManager._texImage2D(GlConst.GL_TEXTURE_2D, 0, GlConst.GL_RGBA, resolution, resolution, 0, GlConst.GL_RGBA, GlConst.GL_UNSIGNED_BYTE, null);
-        return texId;
+        // if (!nodes.isEmpty() && !MinecraftClient.getInstance().isPaused()) {
+        //     System.out.printf("Full time: %d, Memory time: %.2f, Rendering time: %.2f\n", fullTime, memoryTime / (float) fullTime, renderingTime / (float) fullTime);
+        // }
     }
 }
