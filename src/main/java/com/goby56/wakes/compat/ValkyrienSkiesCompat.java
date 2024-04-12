8758c499f1895b6b976370306c617aa3c2001df1
Goby56 <60710855+Goby56@users.noreply.github.com>
2024-04-12 22:43:26 +0200

    hmmm

---
+++ src/main/java/com/goby56/wakes/compat/ValkyrienSkiesCompat.java
@@ -0,0 +1,15 @@
+package com.goby56.wakes.compat;
+
+import net.minecraft.world.World;
+import org.valkyrienskies.core.api.ships.LoadedShip;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+public class ValkyrienSkiesCompat {
+    public static void tick(World world) {
+        // for (LoadedShip ship : VSGameUtilsKt.getShipObjectWorld(world).getLoadedShips()) {
+        //     ship.getShipAABB();
+        // }
+
+
+    }
+}
--- src/main/java/com/goby56/wakes/event/WakeTicker.java
+++ src/main/java/com/goby56/wakes/event/WakeTicker.java
@@ -1,5 +1,6 @@
 package com.goby56.wakes.event;
 
+import com.goby56.wakes.compat.ValkyrienSkiesCompat;
 import com.goby56.wakes.render.SplashPlaneRenderer;
 import com.goby56.wakes.simulation.WakeHandler;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -9,6 +10,7 @@ public class WakeTicker implements ClientTickEvents.EndWorldTick {
     @Override
     public void onEndTick(ClientWorld world) {
         WakeHandler.getInstance().tick();
+        ValkyrienSkiesCompat.tick(world);
         SplashPlaneRenderer.tick();
     }
 }
