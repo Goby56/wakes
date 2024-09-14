16ac0815451fcd3d0c1ff520935e985cc25b9399
Goby56 <60710855+Goby56@users.noreply.github.com>
2024-09-14 16:49:02 +0200

    Fix to #104

--- src/main/java/com/goby56/wakes/mixin/TameableTeleportMixin.java
+++ src/main/java/com/goby56/wakes/mixin/TameableTeleportMixin.java
@@ -3,7 +3,9 @@ package com.goby56.wakes.mixin;
 import com.goby56.wakes.duck.ProducesWake;
 import net.minecraft.entity.ai.goal.FollowOwnerGoal;
 import net.minecraft.entity.passive.TameableEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,10 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(TameableEntity.class)
 public class TameableTeleportMixin {
 
+    @Shadow @Final private TameableEntity tameable;
+
     @Inject(at = @At("TAIL"), method = "tryTeleportTo")
     private void onTeleport(int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
         if (cir.getReturnValue()) {
-            ((ProducesWake) this).setRecentlyTeleported(true);
+            ((ProducesWake) this.tameable).setRecentlyTeleported(true);
         }
     }
 }
